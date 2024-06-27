6f257b73e3abba71e76abbcd78e3d30e8dd84869
Albert Hughes <albert.hughes@amalitech.com>
2024-06-27 15:27:00 +0000

    updated httpPostRequest class

--- src/main/java/httpGetRequests.java
+++ src/main/java/httpGetRequests.java
@@ -16,4 +16,23 @@ public class httpGetRequests {
                 assertThat().statusCode(200).
                 body("data[0].'email'",equalTo("michael.lawson@reqres.in"));
     }
+    @Test
+    public void singleUser() {
+    given().
+            when().
+            get("https://reqres.in/api/users/2").
+            then().
+            assertThat().statusCode(200).
+            body("data.'first_name'",equalTo("Janet")).
+            log().all();
+    }
+    @Test
+    public void notFound() {
+    given().
+            when().
+            get("https://reqres.in/api/unknown/23").
+            then().
+            assertThat().statusCode(404).
+            log().all();
+    }
 }
--- src/main/java/httpPostRequest.java
+++ src/main/java/httpPostRequest.java
@@ -7,16 +7,55 @@ import static io.restassured.RestAssured.given;
 public class httpPostRequest {
     @Test
     public void postRequest() {
-        //https://reqres.in/api/users
         JSONObject postRequest = new JSONObject();
         postRequest.put("name", "Albert Hughes");
         postRequest.put("job", "Developer");
         given()
                 .contentType(ContentType.JSON)
-                .accept(ContentType.JSON).body(postRequest.toJSONString())
+                .accept(ContentType.JSON)
+                .body(postRequest.toJSONString())
                 .when()
                 .post("https://reqres.in/api/users")
                 .then()
                 .assertThat().statusCode(201).log().all();
+    }
+    @Test
+    public void registerUser() {
+        JSONObject register = new JSONObject();
+        register.put("email", "eve.holt@reqres.in");
+        register.put("password", "pistol");
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(register.toJSONString()).
+                when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .assertThat().statusCode(200).log().all();
+
+    }
+    @Test
+    public void login() {
+        JSONObject login = new JSONObject();
+        login.put("email", "eve.holt@reqres.in");
+        login.put("password", "cityslicka");
+        given().contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(login.toJSONString())
+                .when().post("https://reqres.in/api/login")
+                .then()
+                .assertThat().statusCode(200).log().all();
+    }
+    @Test
+    public void unsuccessfulLogin() {
+        JSONObject login = new JSONObject();
+        login.put("email", "peter@klaven");
+        given().contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(login.toJSONString())
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .assertThat().statusCode(400).log().all();
     }
 }
